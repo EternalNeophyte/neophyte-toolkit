f9c635b05337f498a1cca8093f97e612f32f6444
Ilya <180229@edu.psuti.ru>
2021-11-24 20:11:36 +0400

    Minor fixes

--- src/main/java/fluent/Fluent.java
+++ src/main/java/fluent/Fluent.java
@@ -39,6 +39,12 @@ public class Fluent {
         return new TernaryOp<>(routeCondition);
     }
 
+    public static <T> TernaryOp<T> askOptional(boolean routeCondition) {
+        //ToDo реализовать
+        return null;
+    }
+
+
     public static <T> TernaryOp<T> ask(BooleanSupplier routeCondition) {
         return new TernaryOp<>(routeCondition.getAsBoolean());
     }
